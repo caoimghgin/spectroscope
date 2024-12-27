5483f0f19ac826eb87c74619cb7b6c19ed0fa212
Kevin Muldoon <caoimghgin@gmail.com>
2024-12-27 12:01:36 -0500

    Updates

--- main.ts
+++ main.ts
@@ -1,25 +1,30 @@
 import Color, { Coords } from "npm:colorjs.io@0.5.2";
 import { appendFileSync } from "node:fs";
 
-const collection: Color[] = [];
-const metrics = { count: collection.length, tries: 0, added: [0, 0, 0], ms: 0 };
-let startDate = new Date();
-
-
 if (import.meta.main) {
-  await populateCollection();
+
+  let collection: Color[] = [];
+  const metrics = { count: collection.length, tries: 0, added: [0, 0, 0], seconds: 0 };
+  let startDate = new Date();
+  collection = await populateCollection();
+  // console.log(collection)
 
   while (true) {
     startDate = new Date();
     const values = randomLabValues();
     const color = new Color("lab-d65", values as Coords);
-    const result = findInsignificantDifference(color);
+    const result = findInsignificantDifference(collection, color);
     if (!result) {
       collection.push(color);
       appendFileSync("collection.txt", `${values.join("\t")}\r`);
-      const endDate = new Date();
-      metrics.ms = endDate.getTime() - startDate.getTime();
-      console.log(metrics);
+      
+      const now = new Date();
+const formattedTime = now.toLocaleTimeString();
+      var endDate  = new Date();
+      var seconds = (endDate.getTime() - startDate.getTime()) / 1000;
+      metrics.seconds = seconds
+
+      console.log( {formattedTime, ...metrics});
       metrics.added = values;
       metrics.count = collection.length;
       metrics.tries = 0;
@@ -32,18 +37,20 @@ if (import.meta.main) {
 }
 
 async function populateCollection() {
+  const result: Color[] = []
   const contents = await Deno.readTextFile("collection.txt");
   const items = contents.split("\r\n");
   items.forEach((item) => {
     const values = item.split("\t") as Array<T>;
     if (values.length === 3) {
       const color = new Color("lab-d65", values as Coords);
-      collection.push(color);
+      result.push(color)
     }
   });
+  return result
 }
 
-function findInsignificantDifference(color: Color) {
+function findInsignificantDifference(collection: Color[], color: Color) {
   let result: null | Color = null;
   for (const item of collection) {
     const deltaE = item.deltaE(color, "2000");
